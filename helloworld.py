fbef9f1b272ce08e31f6cf33c54a7041fd1ee220
Samson Akpobome <samokpos@yahoo.com>
2022-06-02 20:54:25 -0700

    add city

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,19 @@
-def hello_world():
-    print(f'Hello World!')
+# def hello_world():
+#     print(f'Hello World!')
+
+
+# def main():
+#     hello_world()
+
+
+main()
+
+def hello_world(city):
+    print(f'Hello World {city}!')
 
 
 def main():
-    hello_world()
+    hello_world('NYC')
 
 
 main()
